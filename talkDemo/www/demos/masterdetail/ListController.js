fd00a006362effb74560a2918d212c18bdb7aa6d
Justin James <digitaldrummerj@gmail.com>
2015-07-21 22:07:22 -0700

    added list with delete, reorder, and swipe

--- talkDemo/www/demos/masterdetail/ListController.js
+++ talkDemo/www/demos/masterdetail/ListController.js
@@ -1,57 +1,54 @@
-(function () {
-  "use strict";
-    angular.module('talkDemo')
-    .controller('ListController', ['$scope', 
-        function ($scope) {        
-          $scope.data = {
-            showDelete: false
-          };
-        
-          $scope.itemButtons = [
-            {
-              text: 'Delete',
-              type: 'button-assertive',
-              onTap: function (item) {
-                alert('Delete Item: ' + item.id + ' ?');
-              }
-            }
-          ];
-        
-          $scope.onItemDelete = function (item) {
-            $scope.items.splice($scope.items.indexOf(item), 1);
-          };
-        
-          $scope.items = [
-            { 
-              id: 1
-            },
-            {
-              id: 2
-            },
-            {
-              id: 3
-            },
-            {
-              id: 4
-            },
-            {
-              id: 5
-            },
-            {
-              id: 6
-            },
-            {
-              id: 7
-            },
-            {
-              id: 8
-            },
-            {
-              id: 9
-            },
-            {
-              id: 10
-            }
-          ];
-    }]);
-}());    
+(function() {
+     "use strict";
+     angular.module('talkDemo')
+          .controller('ListController', [
+               '$scope',
+               function($scope) {
+                    $scope.data = {
+                         shouldShowDelete: false,
+                         shouldShowReorder: false,
+                         listCanSwipe: true
+                    };
+
+                    $scope.edit = function(item) {
+                         alert('Edit Item: ' + item.id);
+                    };
+                    $scope.share = function(item) {
+                         alert('Share Item: ' + item.id);
+                    };
+
+                    $scope.moveItem = function(item, fromIndex, toIndex) {
+                         $scope.items.splice(fromIndex, 1);
+                         $scope.items.splice(toIndex, 0, item);
+                    };
+
+                    $scope.onItemDelete = function(item) {
+                         $scope.items.splice($scope.items.indexOf(item), 1);
+                    };
+
+                    $scope.items = [
+                         { id: 0 },
+                         { id: 1 },
+                         { id: 2 },
+                         { id: 3 },
+                         { id: 4 },
+                         { id: 5 },
+                         { id: 6 },
+                         { id: 7 },
+                         { id: 8 },
+                         { id: 9 },
+                    ];
+
+
+                    $scope.angularItems = [
+                         { id: 'Angular Item 1' },
+                         { id: 'Angular Item 2' },
+                         { id: 'Angular Item 3' },
+                         { id: 'Angular Item 4' },
+                         { id: 'Angular Item 5' },
+                         { id: 'Angular Item 6' }
+                    ];
+
+               }
+          ]);
+}());
