b2beb5179db0a667fb3c5009ba1b2c725e1c8c6b
Justin James <digitaldrummerj@gmail.com>
2015-07-22 12:22:14 -0700

    finished pull to refresh

--- talkDemo/www/demos/pulltorefresh/pulltorefreshservice.js
+++ talkDemo/www/demos/pulltorefresh/pulltorefreshservice.js
@@ -1,21 +1,27 @@
-﻿angular.module('talkDemo')
-    .factory('PullToRefreshService', function ($http) {
-         var BASE_URL = "http://api.randomuser.me/";
-         var numberOfContactsToFetch = 5;
+﻿(function() {
+     "use strict";
 
-         var items = [];
-         var n = 0;
-         return {
-              GetContacts: function () {
-                   return $http.get(BASE_URL + '?results=' + numberOfContactsToFetch).then(function (response) {
-                        items = response.data.results;
-                        return items;
-                   });
-              },
-              GetNewContact: function () {
-                   return $http.get(BASE_URL + '?q' + (n++)).then(function (response) {
-                        return response.data.results[0];
-                   });
-              }
-         }
-    });
+     angular.module('talkDemo')
+          .factory('PullToRefreshService', [
+               '$http', function($http) {
+                    var BASE_URL = "http://api.randomuser.me/";
+                    var numberOfContactsToFetch = 5;
+
+                    var items = [];
+                    var n = 0;
+                    return {
+                         GetContacts: function() {
+                              return $http.get(BASE_URL + '?results=' + numberOfContactsToFetch).then(function(response) {
+                                   items = response.data.results;
+                                   return items;
+                              });
+                         },
+                         GetNewContact: function() {
+                              return $http.get(BASE_URL + '?q' + (n++)).then(function(response) {
+                                   return response.data.results[0];
+                              });
+                         }
+                    }
+               }
+          ]);
+}());
